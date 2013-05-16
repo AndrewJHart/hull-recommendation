4532a453c7d455d68428f05527ca3ddbc6534ba8
Xavier Cambar <xcambar@gmail.com>
2013-05-16 14:16:40 +0200

    Syntax cleaning

--- app/widgets/social_md/main.js
+++ app/widgets/social_md/main.js
@@ -1,7 +1,9 @@
+/*global $:true, Hull:true, marked:true */
 Hull.widget('social_md', {
   templates: ['main'],
   datasources: {
     doc: function () {
+      "use strict";
       var _doc = $(this.options.srcSelector).text();
       var markup = marked(_doc);
       return markup;
@@ -11,6 +13,7 @@ Hull.widget('social_md', {
   _selectablesSelector: 'p,h1,h2,h3,h4,h5,h6',
 
   initialize: function () {
+    "use strict";
     var that = this;
     this.$el.on('click', this._selectablesSelector, function () {
       that.toggleAllBut($(this));
@@ -19,12 +22,15 @@ Hull.widget('social_md', {
     });
   },
 
+
   afterRender: function () {
+    "use strict";
     this.$selectables = this.$el.find(this._selectablesSelector);
     this.$selectables.popover({placement: 'bottom', 'title': 'title', trigger:'manual', content: 'Ole'});
   },
 
   toggleAllBut: function (elt) {
+    "use strict";
     this.$selectables.not(elt).popover('hide');
     this.$selectables.not(elt).removeClass('well');
   }
