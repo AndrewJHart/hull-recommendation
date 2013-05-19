e699d96fb99a9bfbe15c031b6176871c91d1f235
Xavier Cambar <xcambar@gmail.com>
2013-05-19 21:08:16 +0200

    Annotate source of popover.js

--- app/scripts/popover.js
+++ app/scripts/popover.js
@@ -1,17 +1,48 @@
+// ## Purpose
+// The purpose of this code is to allow users to recommend (aka 'like or 'upvote')
+// portions of a Markdown document, and show as a popover how many users did so, as well as
+// alongside their (Twitter-based) name and avatar.
+//
+// Generally speaking, it demonstrates how developers can create arbitrary social entities
+// in their app using [hull.io](http://hull.io) to leverage their interactive (ie. social)
+// feature set.
+//
+// ## How is it done ?
+//
+// The [hull.io](http://hull.io) APIs grant developers with the ability to turn
+// any (uniquely defined) object into a featureful social object called `entity`.
+//
+// To do so, the only thing to do is a call to `Hull.data.api('~%SIGNATURE%')` where `%SIGNATURE%`
+// is a base64-encoded version of any data (tipycally, an ID).
+//
+// Entities in [hull.io](http://hull.io) are created lazily, which means you don't have to
+// explicitly create them. If an API call is performed on an entity that does no exist
+// (like we do in this demo), [hull.io](http://hull.io) will assume it is a new entity
+// and will create it before it performs the action defined by the API call.
+//
+// In this demo, we create entities from paragraphs and headings in a Markdown document.
+// The signatures are plain base64 encodings of the `jQuery.fn.html()` evaluation
+// for the selected element.
+
 /*global $:true, Hull:true */
+//
 $(function () {
   "use strict";
-  var container = $('<div>'),
+  var container = $('<div>').appendTo('body'),
+      // The set of HTML elements we want to be able to select and review
       _selectablesSelector = 'p,h1,h2,h3,h4,h5,h6';
 
+  //We delegate click events on the selectables items to the container
+  container.on('click', _selectablesSelector, toggleOnClick);
+
+  // Fetch the markdown file, compile it to HTML and append it to the DOM
   $.ajax('/README.md').then(function (res) {
     var _doc = marked(res);
     container.append(_doc);
-    $('body').append(container);
-    container.on('click', _selectablesSelector, toggleOnClick);
   });
 
 
+  // Here we just make sure that there can not be 2 active elements at the same time.
   function toggleOnClick() {
     $(this).toggleClass('well');
     var isActive = $(this).hasClass('well');
@@ -21,20 +52,33 @@ $(function () {
     }
   }
 
+  //Destroys all the popovers,
+  //sets all the elements matching `_selectableSelector` to the default state.
+  //
+  //The first parameter can be anything accepted by the `.not()` function in jQuery,
+  //and is for excluding some elements from the reset.
   function resetAllExcept(except) {
     container.find(_selectablesSelector).not(except).popover('destroy');
     container.find(_selectablesSelector).not(except).removeClass('well');
   }
 
+  //Registers `click` handlers. `%SIGNATURE%` is a base64-encoded data.
+  //
+  // * `.login` => `Hull.login('twitter')
+  // * `.logout` => `Hull.logout()` _(Actually calls `Hull.logout('twitter')` but the `'twitter'` parameter is not used)_
+  // * `.like` => `Hull.data.api('~%SIGNATURE%/likes', 'post')`
+  // * `.unlike` => `Hull.data.api('~%SIGNATURE%/likes', 'delete')`
+  //
+  // The `.like` (resp. `.unlike`) call could also have been written
+  // `Hull.data.api.post('~%SIGNATURE%/likes')`
+  // (resp. `Hull.data.api.delete('~%SIGNATURE%/likes')`)
+  //
   $.each(['login', 'logout'], function (index, action) {
     container.on('click', '.' + action, function () {
       Hull[action]('twitter');
     });
   });
 
-  /**
-   * Handles Recommendation handlers
-   */
   function addEventListeners(elt, sig) {
     $.each(['like', 'unlike'], function (index, action) {
       var method = action === 'like' ? 'post' : 'delete';
@@ -48,10 +92,13 @@ $(function () {
 
 
 
+  // This is how you can detect whether the current user is logged in regards to [hull.io](http://hull.io)
   function isHullUserLoggedIn () {
     return !!Hull.me.get('identities');
   }
 
+  // We recreate popovers every time they have to be displayed
+  // because their values may have changed (new users may have liked/unliked the targeted entity)
   function createPopOver(elt, sig, contents) {
     addEventListeners(contents, sig);
     elt.popover('destroy');
@@ -59,6 +106,7 @@ $(function () {
     elt.popover('show');
   }
 
+  // Values are applied to the templates
   function applyTemplating(contents, isLiked, count, ppl) {
     contents.find(isLiked ? '.like' : '.unlike').hide();
     contents.find('.countLikes').text(count ? count + ' recommendations' : 'No recommendation');
@@ -73,18 +121,30 @@ $(function () {
   }
 
 
+  // Retrieve an entity. If the entity does not exist in [hull.io](http://hull.io),
+  // it is automatically created. Whether it has to be created or not, an entity is returned by the API call.
+  //
+  // The function returns a pomise.
   function findEntity(_sig) {
     return Hull.data.api('~' + _sig);
   }
 
+  // We want to know who liked our entity.
+  //
+  // Getting to know how many people liked a particular entity is provided as a property
+  // of the entity itself (`my_entity.stats.likes`) and therefore is available
+  // when you retrieve the entity, which means you do not have to manually retrieve
+  // all the users who liked an entity to count them.
   function whoLikedEntity(obj) {
     return Hull.data.api(obj.id + '/likes');
   }
 
+  //Whether the current user (if any) liked the current entity
   function checkUserHasLiked(obj) {
     return Hull.data.api('me/liked/' + obj.id);
   }
 
+  // Retrieve al the data that is required to display the popover correctly
   function fetchHullData(_sig) {
     var entityPromise = findEntity(_sig),
         countLikesPromise = entityPromise.then(function (ent) { return ent.stats.likes || 0; }),
@@ -93,9 +153,11 @@ $(function () {
     return $.when(hasLikedPromise, countLikesPromise, whoLikedPromise);
   }
 
+  //Shows the popover related to the clicked entity
   function showMeLuv(_elt) {
     var $elt = $(_elt),
         loggedIn = isHullUserLoggedIn(),
+        //This how we calculate the signature that will define our entity
         _sig = btoa($elt.html()),
         contents;
 
@@ -111,5 +173,6 @@ $(function () {
     contents.attr('id', null); // This is a clone of an element with an ID
   }
 
+  // This event handler is triggered when the user logs in /out.
   Hull.on('model.hull.me.change', resetAllExcept.bind(undefined, undefined));
 });
