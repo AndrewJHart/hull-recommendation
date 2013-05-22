d6921303e4d8585cccfa3ef4af0d63a56a1ee1c1
Romain Dardour <romain@hull.io>
2013-05-21 22:50:02 -0600

    Use hull demo style

--- app/scripts/popover.js
+++ app/scripts/popover.js
@@ -28,7 +28,7 @@
 //
 $(function () {
   "use strict";
-  var container = $('.container'),
+  var container = $('.readme_container'),
       // The set of HTML elements we want to be able to select and review
       _selectablesSelector = 'p,h1,h2,h3,h4,h5,h6,li';
 
